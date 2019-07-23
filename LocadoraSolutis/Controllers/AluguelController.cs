30148e52ccb3174ae6b7c9066afcb1424dd400db
Marcos Reis <marcossummers@gmail.com>
2019-07-22 22:52:05 -0300

    Início da inserção do React ao Projeto. Apenas a pasta vazia...

--- LocadoraSolutis/Controllers/AluguelController.cs
+++ LocadoraSolutis/Controllers/AluguelController.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿using LocadoraSolutis.Models;
+using LocadoraSolutis.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using LocadoraSolutis.Models;
-using LocadoraSolutis.Repository;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace LocadoraSolutis.Controllers
 {
